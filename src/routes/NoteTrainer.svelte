f87bb21b03adee7c7b07a81d0e8d3c2b1afffdd2
Kevin Wyckmans <451289+kwyckmans@users.noreply.github.com>
2022-07-31 09:50:30 +0000

    create NoteTrainer component

---
+++ src/routes/NoteTrainer.svelte
@@ -0,0 +1,14 @@
+<script>
+    let notes = ["A", "B", "C"];
+    let note = notes[Math.floor(Math.random() * notes.length)];
+</script>
+
+<p>
+    Play {note}
+</p>
+
+<style>
+    p {
+        font-size: 2 em;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import NoteTrainer from "./NoteTrainer.svelte";
+</script>
+
+<h1>Let's do some practice!</h1>
+<NoteTrainer />
