91fd5fd649ce739329fc377144df7f5b3a00cb71
Kevin Wyckmans <451289+kwyckmans@users.noreply.github.com>
2022-07-31 10:00:39 +0000

    refresh note on keypress

--- src/routes/NoteTrainer.svelte
+++ src/routes/NoteTrainer.svelte
@@ -1,10 +1,25 @@
 <script>
     let notes = ["A", "B", "C"];
-    let note = notes[Math.floor(Math.random() * notes.length)];
+    let note = selectNote();
+    let key = undefined;
+
+    function selectNote() {
+        return notes[Math.floor(Math.random() * notes.length)];
+    }
+
+    function handleKeyPress(event) {
+        console.log("presed a key", event);
+        note = selectNote();
+        key = event.key;
+    }
 </script>
+
+<svelte:window on:keypress={handleKeyPress} />
 
 <p>
     Play {note}
+
+    pressed {key}
 </p>
 
 <style>
